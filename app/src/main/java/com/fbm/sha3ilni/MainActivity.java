8289be5c474b909edf0c9d031897f0c521481363
muja555 <mujahedD5@gmail.com>
2016-05-13 16:31:05 +0300

    init commit

---
+++ app/src/main/java/com/fbm/sha3ilni/MainActivity.java
@@ -0,0 +1,24 @@
+package com.fbm.sha3ilni;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameField, passwordField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameField = (EditText) findViewById(R.id.username);
+
+        passwordField = (EditText) findViewById(R.id.password);
+
+        usernameField.setTypeface(Typeface.createFromAsset(getAssets(), "stc.otf"));
+
+    }
+}
