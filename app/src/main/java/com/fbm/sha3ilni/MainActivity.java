14e71981080e9b3496e1943208fcb75f037000b2
barajallad <bara.jallad@gmail.com>
2016-05-13 18:25:40 +0300

    layout

--- app/src/main/java/com/fbm/sha3ilni/MainActivity.java
+++ app/src/main/java/com/fbm/sha3ilni/MainActivity.java
@@ -3,13 +3,22 @@ package com.fbm.sha3ilni;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText usernameField, passwordField;
 
+
+    Button b1,b2;
+    int count=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
 
         usernameField.setTypeface(Typeface.createFromAsset(getAssets(), "stc.otf"));
 
+        b1=(Button)findViewById(R.id.button);
+        usernameField=(EditText)findViewById(R.id.username);
+        passwordField=(EditText)findViewById(R.id.password);
+
+        b2=(Button)findViewById(R.id.button1);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(usernameField.getText().toString().equals("admin") &&
+
+                        passwordField.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
-}
+    }
+
